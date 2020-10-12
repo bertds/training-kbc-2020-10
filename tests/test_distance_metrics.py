fc73dd96bb23fb17fb3db6ddface2808b4d3d9d6
Oliver Willekens <oliver.willekens@dataminded.be>
2020-10-12 16:31:22 +0200

    feat: add task for unit test

---
+++ tests/test_distance_metrics.py
@@ -0,0 +1,16 @@
+"""
+Exercise: implement a series of tests with which you validate the
+correctness (or lack thereof) of the function great_circle_distance.
+"""
+import math
+
+import pytest
+
+from exercises.b_unit_test_demo.distance_metrics import great_circle_distance
+
+
+def test_great_circle_distance():
+    # Write out at least two tests for the great_circle_distance function.
+    # Use these to answer the question: is the function correct?
+
+    pass
