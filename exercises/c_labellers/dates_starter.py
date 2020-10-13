fe0ec978b2d2d83fb5a2e4cd622496dad6fc5ff8
Oliver Willekens <oliver.willekens@dataminded.be>
2020-10-13 09:57:16 +0200

    added material from session 1

---
+++ exercises/c_labellers/dates_starter.py
@@ -0,0 +1,16 @@
+def label_weekend():
+    """Given a Spark DataFrame, and the name of a column inside that DataFrame,
+    add a column that indicates whether the value in that column represents a
+    day in the weekend."""
+    pass
+
+
+def is_belgian_holiday():
+    """Given a datetime.date instance, return whether it is a Belgian holiday."""
+    pass
+
+
+def label_holidays():
+    """Add a column to a DataFrame indicating whether or not the column `colname`
+    is a holiday."""
+    pass
