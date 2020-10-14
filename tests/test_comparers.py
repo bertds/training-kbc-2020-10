64cd884a030fb1fc64868a8b755544a612045fd6
Oliver Willekens <oliver.willekens@dataminded.be>
2020-10-14 14:23:49 +0200

    split tests by functionality

--- tests/test_comparers.py
+++ tests/test_comparers.py
@@ -7,45 +7,57 @@ from .comparers import assert_frames_functionally_equivalent
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
 
-def test_frames_functionally_equivalent():
-
+def test_non_equal_frames_not_functionally_equivalent():
     frame1 = spark.createDataFrame([(1,), (2,)])
-    frame1.printSchema()
     frame2 = spark.createDataFrame([(1,), (2,), (2,)])
 
     with pytest.raises(AssertionError):
         assert_frames_functionally_equivalent(frame1, frame2)
 
-    # Identical frames are… well, identical.
-    assert_frames_functionally_equivalent(frame1, frame1)
 
+def test_identical_frames_are_identical():
+    df = spark.range(1)
+    assert_frames_functionally_equivalent(df, df)
+
+
+def test_column_order_is_irrelevant_for_functional_equivalence():
     fields = [
         StructField("name", StringType(), True),
         StructField("age", IntegerType(), True),
     ]
-    frame3 = spark.createDataFrame(
+    frame1 = spark.createDataFrame(
         [("Wim", 1), ("Conrad", 2)], schema=StructType(fields)
     )
 
-    frame4 = spark.createDataFrame(
+    frame2 = spark.createDataFrame(
         [(1, "Wim"), (2, "Conrad")], schema=StructType(fields[::-1])
     )
-    # Ordering of the columns is not important
-    assert_frames_functionally_equivalent(frame3, frame4)
+    assert_frames_functionally_equivalent(frame1, frame2)
 
-    # in Python 3, None is not sortable wrt to other types,
-    # i.e. None < 3 errors out
-    frame5 = spark.createDataFrame(
-        [(None, 1), ("Christina", 2)], schema=StructType(fields)
-    )
-    assert_frames_functionally_equivalent(frame5, frame5)
 
-    # Ordering of the data is not important
-    frame6 = spark.createDataFrame(
+def test_ordering_of_data_is_irrelevant_for_functional_equivalence():
+    fields = [
+        StructField("name", StringType(), True),
+        StructField("age", IntegerType(), True),
+    ]
+    frame1 = spark.createDataFrame(
         [("Wim", 1), ("Conrad", 2)], schema=StructType(fields)
     )
 
-    frame7 = spark.createDataFrame(
+    frame2 = spark.createDataFrame(
         [("Conrad", 2), ("Wim", 1)], schema=StructType(fields)
     )
-    assert_frames_functionally_equivalent(frame6, frame7)
+    assert_frames_functionally_equivalent(frame1, frame2)
+
+
+def test_functional_equivalence_testing_works_with_nones():
+    # in Python 3, None is not sortable wrt to other types,
+    # i.e. None < 3 errors out
+    fields = [
+        StructField("name", StringType(), True),
+        StructField("age", IntegerType(), True),
+    ]
+
+    df = spark.createDataFrame([(None, 1), ("Christina", 2)], schema=StructType(fields))
+    assert_frames_functionally_equivalent(df, df)
+
